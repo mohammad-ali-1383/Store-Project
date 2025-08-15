f4884d6016a19b92f0d0389a42d9aadc1d0ecc16
mohammad ali ghorbanzadeh <ghorbanzadehtv@gmail.com>
2025-08-15 22:18:13 +0330

    Compatible with React 19

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,16 +1,32 @@
-import { RotatingLines } from "react-loader-spinner";
+//or react19
+import { ClipLoader } from "react-spinners";
 import styles from "./Loader.module.css";
+
 function Loader() {
   return (
     <div className={styles.loader}>
-      <RotatingLines
-        width="100px"
-        height="100px"
-        strokeColor="#fe5d42"
-        strokeWidth="3"
-      />
+      <ClipLoader size={100} color="#fe5d42" />
     </div>
   );
 }
 
 export default Loader;
+
+//fo react18
+// import { RotatingLines } from "react-loader-spinner";
+
+// import styles from "./Loader.module.css";
+// function Loader() {
+//   return (
+//     <div className={styles.loader}>
+//       <RotatingLines
+//         width="100px"
+//         height="100px"
+//         strokeColor="#fe5d42"
+//         strokeWidth="3"
+//       />
+//     </div>
+//   );
+// }
+
+// export default Loader;
